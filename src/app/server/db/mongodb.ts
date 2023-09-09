2513b1c5085deda9efdbd94bfc5c445cde9157be
Sergio <52042600+SergioFC1989@users.noreply.github.com>
2023-09-09 03:11:28 +0200

    [17]: feat: Create endpoint to get counties (#28)
    * feat: created endpoint counties. rename folder verbs
    
    * chore: moved location folders
    
    * chore: relocated folders for the new API configuration
    
    * chore: removed console.log
    
    * chore: install dependences
    
    * chore: try deploy vercel
    
    * fix: added node to package.json
    
    * chore: try deploy in vercel
    
    * chore: uninstall dotenv
    
    * chore: serialized params for request to mongodb
    
    * feat: added new config to vercel.json
    
    * chore: modified config vercel.json
    
    * chore: modified config vercel.json
    
    * feat: install dependences
    
    ---------
    
    Co-authored-by: Sergio FCU <sergiofcu@ext.inditex.com>

--- src/app/server/db/mongodb.ts
+++ src/app/server/db/mongodb.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
 import { Db, MongoClient, ServerApiVersion } from "mongodb";
-
-dotenv.config();
 
 const client = new MongoClient(process.env.MONGODB_URI ?? "", {
   serverApi: {
--- src/app/server/utils/verbs.ts
+++ src/app/server/utils/verbs.ts
@@ -1,6 +1,23 @@
 import type { NextApiRequest } from "next";
 import { connectDatabaseMongoDB } from "../db/mongodb";
 
+type ParamsProps = {
+  [key: string]: any;
+};
+
+const serializerParams = (request: Request) => {
+  let params: ParamsProps = {}
+  const { searchParams } = new URL(request.url)
+  if(searchParams.size === 0) {
+    return undefined
+  }else {
+    searchParams.forEach((value: string, key: string) => {
+      params = { ...params, [key]: value }
+    })
+    return params
+  }
+}
+
 /**
  * Retrieves data from a MongoDB collection based on the provided collection name and request query parameters.
  * @param collection - The name of the MongoDB collection from which to retrieve data.
@@ -10,19 +27,20 @@ import { connectDatabaseMongoDB } from "../db/mongodb";
 
 export const fetchGET = async (
   collection: string,
-  req: Pick<NextApiRequest, "query">
+  request: Request
 ) => {
-  const params = Object.keys(req.query).length > 0 ? req.query : undefined;
+  const serializedParams: ParamsProps | undefined = serializerParams(request);
   const db: any = await connectDatabaseMongoDB();
-  const response = await db.collection(collection).find(params).toArray();
+  const response = await db.collection(collection).find(serializedParams).toArray();
   return response;
 };
 
+//TODO: Adaptar parametro cuando usemos este verbo
 export const fetchPOST = async (
   collection: string,
   req: Pick<NextApiRequest, "body">
 ) => {
-  const typeInsert = typeof req.body === "object" ? "insertOne" : "insertMany";
+  const typeInsert = !Array.isArray(req.body) ? "insertOne" : "insertMany";
   const db: any = await connectDatabaseMongoDB();
   const response = await db.collection(collection)[typeInsert](req.body);
   return response;

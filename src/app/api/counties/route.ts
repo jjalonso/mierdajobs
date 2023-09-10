2f84f31debf459d0a02ce0f24cae5e394073b8d9
SergioFC <sergiofcu@ext.inditex.com>
2023-09-10 05:18:29 +0200

    feat: modified method fetchPOST. added POST in API

--- src/app/api/counties/route.ts
+++ src/app/api/counties/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server";
-import { fetchGET } from "../../server/utils/verbs";
+import { fetchGET, fetchPOST } from "../../server/utils/verbs";
+
+const collection = "counties";
 
 export const GET = async (request: Request) => {
-  const response = await fetchGET("counties", request);
+  const response = await fetchGET(collection, request);
+  return NextResponse.json(response);
+};
+
+export const POST = async (request: Request) => {
+  const response = await fetchPOST(collection, request);
   return NextResponse.json(response);
 };
--- src/app/server/utils/verbs.ts
+++ src/app/server/utils/verbs.ts
@@ -1,22 +1,25 @@
 import type { NextApiRequest } from "next";
-import { connectDatabaseMongoDB } from "../db/mongodb";
+import {
+  connectDatabaseMongoDB,
+  disconnectDatabaseMongoDB,
+} from "../db/mongodb";
 
 type ParamsProps = {
   [key: string]: any;
 };
 
 const serializerParams = (request: Request) => {
-  let params: ParamsProps = {}
-  const { searchParams } = new URL(request.url)
-  if(searchParams.size === 0) {
-    return undefined
-  }else {
+  let params: ParamsProps = {};
+  const { searchParams } = new URL(request.url);
+  if (searchParams.size === 0) {
+    return undefined;
+  } else {
     searchParams.forEach((value: string, key: string) => {
-      params = { ...params, [key]: value }
-    })
-    return params
+      params = { ...params, [key]: value };
+    });
+    return params;
   }
-}
+};
 
 /**
  * Retrieves data from a MongoDB collection based on the provided collection name and request query parameters.
@@ -25,23 +28,24 @@ const serializerParams = (request: Request) => {
  * @returns The retrieved data from the MongoDB collection or an error message along with the error object.
  */
 
-export const fetchGET = async (
-  collection: string,
-  request: Request
-) => {
+export const fetchGET = async (collection: string, request: Request) => {
   const serializedParams: ParamsProps | undefined = serializerParams(request);
   const db: any = await connectDatabaseMongoDB();
-  const response = await db.collection(collection).find(serializedParams).toArray();
+  const response = await db
+    .collection(collection)
+    .find(serializedParams)
+    .toArray();
+  await disconnectDatabaseMongoDB();
   return response;
 };
 
-//TODO: Adaptar parametro cuando usemos este verbo
-export const fetchPOST = async (
-  collection: string,
-  req: Pick<NextApiRequest, "body">
-) => {
-  const typeInsert = !Array.isArray(req.body) ? "insertOne" : "insertMany";
+export const fetchPOST = async (collection: string, request: Request) => {
+  const adaptedRequest = await request.json();
+  const typeInsert = !Array.isArray(adaptedRequest)
+    ? "insertOne"
+    : "insertMany";
   const db: any = await connectDatabaseMongoDB();
-  const response = await db.collection(collection)[typeInsert](req.body);
+  const response = await db.collection(collection)[typeInsert](adaptedRequest);
+  await disconnectDatabaseMongoDB();
   return response;
 };

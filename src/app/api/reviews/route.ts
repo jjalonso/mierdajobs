e8b092233428ae1e0dc854d3b4e8c22e0cd5c1ba
SergioFCU <sergiofcu@ext.inditex.com>
2023-09-21 23:46:03 +0200

    feat: added handle errors in review

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server";
 import { insertReview } from "./actions";
+import { handleErrors } from "../utils";
 
 export const POST = async (request: Request) => {
-  const bodyObject = await request.json();
-  const response = await insertReview(bodyObject);
-  return NextResponse.json(response);
+  try {
+    const bodyObject = await request.json();
+    const response = await insertReview(bodyObject);
+    return NextResponse.json(response);
+  } catch (error) {
+    return handleErrors("Hubo un problema al insertar la reseña", 500);
+  }
 };

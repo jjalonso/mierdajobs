f425d4735237e43e9768db24946d2281786f1b44
Juan Jose Alonso <kernel.no.found@gmail.com>
2023-10-31 23:16:44 +0000

    test

--- src/app/api/auth/auth-options.ts
+++ src/app/api/auth/auth-options.ts
@@ -1,8 +1,6 @@
 import { NextAuthOptions, Session, User } from "next-auth";
-import EmailProvider, { SendVerificationRequestParams } from "next-auth/providers/email";
-import nodemailer from "nodemailer"
+import EmailProvider from "next-auth/providers/email";
 
-import { emailHtml, emailText } from "./email";
 import { ExtendedAdapter } from "./extended-adapter";
 
 import clientPromise from "@/lib/mongodb/client";
@@ -19,28 +17,28 @@ const authOptions = {
   debug: true,
   providers: [
     EmailProvider({
-      sendVerificationRequest: async (params: SendVerificationRequestParams) => {
-        console.log("sendverificationrequest")
-        const { identifier, url, provider } = params
-        const { host } = new URL(url)
-        // NOTE: You are not required to use `nodemailer`, use whatever you want.
-        const transport = nodemailer.createTransport(provider.server)
-        console.log("send email")
+      // sendVerificationRequest: async (params: SendVerificationRequestParams) => {
+      //   console.log("sendverificationrequest")
+      //   const { identifier, url, provider } = params
+      //   const { host } = new URL(url)
+      //   // NOTE: You are not required to use `nodemailer`, use whatever you want.
+      //   const transport = nodemailer.createTransport(provider.server)
+      //   console.log("send email")
 
-        const result = await transport.sendMail({
-          to: identifier,
-          from: provider.from,
-          subject: `Sign in to ${host}`,
-          text: emailText({ url, host }),
-          html: emailHtml({ url, host }),
-        })
-        console.log("email sent")
+      //   const result = await transport.sendMail({
+      //     to: identifier,
+      //     from: provider.from,
+      //     subject: `Sign in to ${host}`,
+      //     text: emailText({ url, host }),
+      //     html: emailHtml({ url, host }),
+      //   })
+      //   console.log("email sent")
 
-        const failed = result.rejected.concat(result.pending).filter(Boolean)
-        if (failed.length) {
-          throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`)
-        }
-      },
+      //   const failed = result.rejected.concat(result.pending).filter(Boolean)
+      //   if (failed.length) {
+      //     throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`)
+      //   }
+      // },
       server: {
         host: process.env.EMAIL_SERVER_HOST,
         port: Number(process.env.EMAIL_SERVER_PORT),

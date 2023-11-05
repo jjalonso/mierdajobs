c641e9efd98c66813d8f2d23eb4ec0a08aab3d10
Juan Jose A <jjalonso@users.noreply.github.com>
2023-11-05 01:12:56 +0000

    Revert "DRAFT: [43]: feat: Amend send review (#69)"
    This reverts commit a05ced5545dd51c7bdb759255c11c30d809ac7f0.

--- src/app/api/_reviews/send-review/actions.ts
+++ src/app/api/_reviews/send-review/actions.ts
@@ -1,35 +1,10 @@
 "use server";
 
-import xss from "xss";
-
-import { schemaReviews } from "./schema";
 import { SendReviewRequest } from "./types";
+import { processBody } from "./utils";
 
-import { disconnectDB } from "@/app/_server/db/mongodb";
 import { insertDataInCollection } from "@/app/_server/db/verbs";
 
 export const sendReview = async (review: SendReviewRequest) => {
-  let customError = "Error al enviar la reseña";
-
-  try {
-    const { error, value } = schemaReviews.validate(review);
-
-    if (error) {
-      customError = error.message;
-      throw error;
-    }
-
-    const sanitizedComment = xss(value.comment);
-
-    if (sanitizedComment !== value.comment) {
-      customError = "El comentario tiene contenido no permitido o malicioso";
-      throw error;
-    }
-
-    await insertDataInCollection("reviews", review);
-  } catch (error) {
-    throw new Error(customError);
-  } finally {
-    await disconnectDB();
-  }
-};
+	await insertDataInCollection("reviews", processBody(review));
+}
